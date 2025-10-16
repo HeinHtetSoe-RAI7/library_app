701641040038fd8c4540f10104fd8a639b07067b
HeinHtetSoe-RAI7 <67011688@kmitl.ac.th>
2025-10-17 01:43:51 +0700

    Commit

---
+++ nextjs/next.config.mjs
@@ -0,0 +1,31 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  allowedDevOrigins: ["*"],
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // all routes
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
